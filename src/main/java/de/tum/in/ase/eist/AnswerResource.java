3e1e727ee6f034d575fe15da9b6463a7bbc98650
ta <hgiweods@gmx.de>
2022-07-13 10:27:28 +0200

    empty commit

--- src/main/java/de/tum/in/ase/eist/AnswerResource.java
+++ src/main/java/de/tum/in/ase/eist/AnswerResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "api", produces = {MediaType.TEXT_PLAIN_VALUE})
 public class AnswerResource {
 
+
 	private final Logger logger = LoggerFactory.getLogger(AnswerResource.class);
 
 	private final QueryProcessor queryProcessor;
