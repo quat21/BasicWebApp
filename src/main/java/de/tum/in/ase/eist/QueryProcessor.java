f842f987d08e1e5684b34319b10af47971c96579
ta <hgiweods@gmx.de>
2022-07-13 10:58:22 +0200

    test

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -13,7 +13,34 @@ public class QueryProcessor {
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
            return "ta";
-        } else { // TODO extend the programm here
+        } else if (query.contains("which of the following numbers is the largest")) {
+            String[] words = query.replaceAll(",", "").split(" ");
+            boolean record = false;
+            int largest = -1;
+            for (int i = 0; i < words.length; i++) {
+                if (record) {
+                    if (Integer.parseInt(word) > largest) {
+                        largest = Integer.parseInt(word);
+                    }
+                }
+                if (words[i].equals("largest:")) {
+                    record = true;
+                }
+            }
+            return String.valueOf(largest);
+        }  else if (true) {
+            return "";
+        } else if (true) {
+            return "";
+        } else if (true) {
+            return "";
+        } else if (true) {
+            return "";
+        } else if (true) {
+            return "";
+        } else if (true) {
+            return "";
+        } else {
             return "";
         }
     }
